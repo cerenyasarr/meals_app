6e6e3d2a5321a34dd13b83a7bdec7982cf9b2cec
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-26 23:32:24 +0300

    feat: create CategoriesScreen with basic AppBar

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Pick your category')));
+  }
+}
