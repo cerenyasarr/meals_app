96df32013467baff229a1d8093ae5077617d26b2
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-01 23:51:13 +0300

    style: apply theme styling to empty state message

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/main.dart';
 
 import 'package:meals_app/models/meal.dart';
 
@@ -11,19 +12,29 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-        itemBuilder: (ctx, index) => Text(meals[index].title),
-      );
+      itemBuilder: (ctx, index) => Text(meals[index].title),
+    );
 
     if (meals.isEmpty) {
-      content = Center(child: Column(mainAxisSize: MainAxisSize.min,
-      children: [
-        Text('Uh oh ... nothing here!'),
-        SizedBox(height: 16),
-        Text('Try selecting a different category!'),
-      ],),);
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Uh oh ... nothing here!'),
+            SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+          ],
+        ),
+      );
     }
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: 
+      body: content,
+    );
   }
 }
