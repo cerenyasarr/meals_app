ff691a9c46aeff96e7ac9ef116202dfa62e8fe99
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-05 00:40:36 +0300

    style: apply custom ARGB color to 'German' category

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -12,7 +12,11 @@ const availableCategories = [
   ),
   Category(id: 'c2', title: 'Quick & Easy', color: Colors.red),
   Category(id: 'c3', title: 'Hamburgers', color: Colors.orange),
-  Category(id: 'c4', title: 'German', color: Colors.amber),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Color.fromARGB(255, 226, 202, 133),
+  ),
   Category(id: 'c5', title: 'Light & Lovely', color: Colors.blue),
   Category(id: 'c6', title: 'Exotic', color: Colors.green),
   Category(
