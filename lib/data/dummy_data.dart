dce81ae2aa5a3d81b396ba0d3f075facf1729fb8
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-05 23:58:05 +0300

    fix: update dummy_dart.dart

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -38,7 +38,7 @@ const dummyMeals = [
     complexity: Complexity.simple,
     imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
-    duration: 20,
+    duration: 19,
     ingredients: [
       '4 Tomatoes',
       '1 Tablespoon of Olive Oil',
