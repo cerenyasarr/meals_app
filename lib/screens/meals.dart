b6f9b54b3ae0d1b75d222883fb0a270e016abced
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-07 00:04:53 +0300

    feat: set itemCount dynamically based on meals length

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -39,6 +39,7 @@ class MealsScreen extends StatelessWidget {
 
       if (meals.isNotEmpty) {
         content = ListView.builder(
+          itemCount: meals.length,
           itemBuilder: (ctx, index) => Text(meals[index].title),
         );
       }
