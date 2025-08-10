04b3ecbbdd78d5770e65cae7a7ef1c3030928794
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-11 00:07:38 +0300

    feat: add navigation to MealsScreen on category selection

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   void _selectCategory(BuildContext context) {
-    Navigator.of(context).push(route); // Navigator.push(context, route)
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'Some title', meals: []),
+      ),
+    ); // Navigator.push(context, route)
   }
 
   @override
