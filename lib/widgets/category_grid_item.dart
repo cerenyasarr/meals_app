4ebd9f2b402e35e673e3c0be22dd31786c0f7f31
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-11 00:10:12 +0300

    feat: add onSelectCategory to CategoryGridItem

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
