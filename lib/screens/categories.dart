d19dfe5235b98dc82f10e6e5986498c71733f91e
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-10 23:58:25 +0300

    feat: add category selection navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,6 +5,10 @@ import 'package:meals_app/widgets/category_grid_item.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(route);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
