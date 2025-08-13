29e31506ab5dd68f5a76bd80e35e4e6be88c0a98
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-13 23:47:32 +0300

    style: expand positioned container to right edge

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -19,7 +19,7 @@ class MealItem extends StatelessWidget {
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
             ),
-            Positioned(bottom: 0, left: 0, child: Container()),
+            Positioned(bottom: 0, left: 0, right: 0, child: Container()),
           ],
         ),
       ),
