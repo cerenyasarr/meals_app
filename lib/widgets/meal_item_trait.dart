fc787540552f0240083a57c256293a8f35324010
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-15 23:50:42 +0300

    feat: add MealItemTrait widget skeleton

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class MealItemTrait extends StatelessWidget {
+  const MealItemTrait();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
