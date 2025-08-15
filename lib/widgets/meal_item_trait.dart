deb1f753f2d40ab5288b89762bf6d45d7c09d713
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-15 23:56:58 +0300

    feat: add props to MealItemTrait widget

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 class MealItemTrait extends StatelessWidget {
-  const MealItemTrait();
+  const MealItemTrait({super.key, required this.icon, required this.label,
+  });
+
+  final IconData icon;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Row(children: [
+      Icon(),
+      SizedBox(),
+      Text(data),
+    ],)
   }
 }
