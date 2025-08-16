0283147e5c2801b988beaf8856aaf9529deebec8
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-17 00:03:58 +0300

    refactor: remove redundant comment line

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -46,7 +46,7 @@ class MealItem extends StatelessWidget {
                       maxLines: 2,
                       textAlign: TextAlign.center,
                       softWrap: true,
-                      overflow: TextOverflow.ellipsis, // Very long text...
+                      overflow: TextOverflow.ellipsis,
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
