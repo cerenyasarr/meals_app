4eca1d4853f42cf2aa005b4a81fc061c82d23113
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-18 23:55:28 +0300

    refactor: update icon styling

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -11,7 +11,7 @@ class MealItemTrait extends StatelessWidget {
     return Row(
       children: [
         Icon(icon, size: 15, color: Colors.white),
-        SizedBox(width: 6),
+        const SizedBox(width: 6),
         Text(label, style: TextStyle(color: Colors.white)),
       ],
     );
