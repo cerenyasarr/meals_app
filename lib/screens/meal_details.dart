a3afea021cc0a5654d48525318fe3ca267e13e89
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-21 23:47:48 +0300

    feat: add Meal field to MealDetails

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealDetails extends StatelessWidget {
   const MealDetails({super.key});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
