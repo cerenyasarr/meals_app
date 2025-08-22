0fee8ace22f2b61336afb25291dc3061f915886b
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-22 23:52:09 +0300

    feat: add onSelectMeal callback to MealDetailsScreen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -5,9 +5,10 @@ import 'package:transparent_image/transparent_image.dart';
 import 'package:meals_app/widgets/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
