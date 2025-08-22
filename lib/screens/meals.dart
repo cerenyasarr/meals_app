27b2f6208163e3015606e7418b5d20ad3fda4a9c
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-23 00:30:36 +0300

    feat: add selectMeal method for navigation

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -9,8 +10,12 @@ class MealsScreen extends StatelessWidget {
   final String title;
   final List<Meal> meals;
 
-  void selectMeal(Meal meal){}
-  
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal)));
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
@@ -42,8 +47,12 @@ class MealsScreen extends StatelessWidget {
       if (meals.isNotEmpty) {
         content = ListView.builder(
           itemCount: meals.length,
-          itemBuilder: (ctx, index) => MealItem(meal: meals[index],
-          onSelectMeal: ,),
+          itemBuilder: (ctx, index) => MealItem(
+            meal: meals[index],
+            onSelectMeal: (meal) {
+              selectMeal(context, meal);
+            },
+          ),
         );
       }
     }
