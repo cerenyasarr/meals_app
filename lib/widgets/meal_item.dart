027d5fdfacb88e4cede5bf9a097ccac4cb7b3d34
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-22 23:57:14 +0300

    feat: trigger onSelectMeal callback on tap

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -30,7 +30,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
