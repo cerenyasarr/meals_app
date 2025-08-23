15894ffb549e8dc2fc5a816e66ebb12e5d4ec6b4
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-23 23:41:08 +0300

    style: add vertical spacing with SizedBox

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -18,6 +18,7 @@ class MealDetailsScreen extends StatelessWidget {
             width: double.infinity,
             fit: BoxFit.cover,
           ),
+          const SizedBox(height: 14),
         ],
       ),
     );
