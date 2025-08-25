4ccaa65331e74dd88f057ce405ca6d91848c6767
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-25 23:49:20 +0300

    style: adjust widget margin

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -23,7 +23,7 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(8),
+      margin: const EdgeInsets.all(7),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadiusGeometry.circular(8),
       ),
