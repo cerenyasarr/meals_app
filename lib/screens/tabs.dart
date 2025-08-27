129001fe13d7a623e1fdf2da2e8f8d8f2c848bb8
Ceren Yaşar <cerenysr1@gmail.com>
2025-08-28 00:11:18 +0300

    feat: add TabsScreen StatefulWidget

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
