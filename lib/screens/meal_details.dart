3070f7eb89bbce3545537431003e7e5fe62f2e0c
Ceren Yaşar <cerenysr1@gmail.com>
2025-09-04 23:55:19 +0300

    feat: add dynamic favorite star icon in MealDetailScreen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -16,7 +16,14 @@ class MealDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.star))],
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon: const Icon(Icons.star),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
