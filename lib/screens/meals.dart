7fd2a790f9759fb524300bf942ed269ebc3ae63a
Ceren Yaşar <cerenysr1@gmail.com>
2025-09-05 00:03:55 +0300

    feat: add onToggleFavorites callback

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -5,10 +5,16 @@ import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, this.title, required this.meals});
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+    required this.onToggleFavorites,
+  });
 
   final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorites;
 
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.of(
