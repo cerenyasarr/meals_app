ee2e04850e1e9809e1ddb404241646292cd7bdb5
Ceren Yaşar <cerenysr1@gmail.com>
2025-09-20 23:52:37 +0300

    style: adjust category tile background opacity

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -25,7 +25,7 @@ class CategoryGridItem extends StatelessWidget {
           gradient: LinearGradient(
             colors: [
               category.color.withValues(alpha: 0.55),
-              category.color.withValues(alpha: 0.8),
+              category.color.withValues(alpha: 0.9),
             ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
