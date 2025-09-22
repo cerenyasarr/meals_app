b9447d9008990e749e99ce337496200dd4163e21
Ceren Yaşar <cerenysr1@gmail.com>
2025-09-23 00:00:26 +0300

    refactor: remove Drawer widget from build method

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 class MainDrawer extends StatelessWidget {
   const MainDrawer({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: 
+ 
       
     ),
   }
