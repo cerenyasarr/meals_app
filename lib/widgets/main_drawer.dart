e4f2715a1a0609fe53cdd03afd9d39b23b1ae4e7
Ceren Yaşar <cerenysr1@gmail.com>
2025-09-24 23:47:44 +0300

    refactor: update gradient alignment

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -10,14 +10,18 @@ class MainDrawer extends StatelessWidget {
         children: [
           DrawerHeader(
             padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(gradient: LinearGradient(
-              colors: [
-                Theme.of(context).colorScheme.primaryContainer,
-                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8),
-              ],
-              begin: ,
-              end: 
-              ),),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primaryContainer,
+                  Theme.of(
+                    context,
+                  ).colorScheme.primaryContainer.withOpacity(0.8),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
             child: child,
           ),
         ],
